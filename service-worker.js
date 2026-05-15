8d6cf9552d4a19d029f4ea8f7686a9370cb723ee
mfrank1977 <m.frank1977@icloud.com>
2026-05-15 20:19:19 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,17 @@
+const CACHE_NAME = 'konfliktberater-v2';
+const FILES = ['./', './index.html', './manifest.json'];
+
 self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
